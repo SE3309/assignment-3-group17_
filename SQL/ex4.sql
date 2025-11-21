773c431f15fbab2b648349ca5b9b4d38e3625b22
Hamish <hdejong4@uwo.ca>
2025-11-21 15:07:21 -0500

    Added sql file to view data for part 4 of the assignment

---
+++ SQL/ex4.sql
@@ -0,0 +1,15 @@
+SELECT * FROM tutor LIMIT 5;
+SELECT * FROM student LIMIT 5;
+SELECT * FROM course LIMIT 5;
+SELECT * FROM location LIMIT 5;
+
+SELECT * FROM tutor_course LIMIT 5;
+SELECT * FROM student_courses LIMIT 5;
+
+SELECT * FROM tutor_availability LIMIT 5;
+SELECT * FROM student_availability LIMIT 5;
+SELECT * FROM availability LIMIT 5;
+
+SELECT * FROM booking LIMIT 5;
+SELECT * FROM review LIMIT 5;
+SELECT * FROM payment LIMIT 5;
